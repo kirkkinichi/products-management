358836ef9ff23817f4a52b9662abad8f273cb1a0
Kirk Silva <kirk.ki@hotmail.com>
2025-01-25 12:32:36 -0300

    added product entity

---
+++ backend/src/products/products.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    codprod: number;
+
+    @Column()
+    dscrprod: string;
+
+    @Column()
+    marca: string;
+
+    @Column()
+    valor: string;
+}
